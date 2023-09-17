6c8577cce7adc9494b0a91426f0ce8c3d20d7916
Benjamin <benjaminpalko@hotmail.com>
2023-09-17 17:38:22 -0400

    Player/Camera controllers, added player actions as well as base scenes and a debug setup

---
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField] private float movementSpeed = 1.0f;
+    
+    private Vector3 m_Movement;
+    
+    private void Update() {
+        if (m_Movement == Vector3.zero) return;
+        var position = transform.position;
+        position = Vector3.MoveTowards(position, position + m_Movement,
+            Time.deltaTime * movementSpeed);
+        transform.position = position;
+    }
+
+    public void OnMove(InputValue inputValue) {
+        Debug.Log("Movement input");
+        m_Movement = inputValue.Get<Vector2>();
+    }
+
+    public void OnA() {
+        Debug.Log("A Pressed");
+    }
+
+    public void OnB() {
+        Debug.Log("B Pressed");
+    }
+
+    public void OnSelect() {
+        Debug.Log("Select Pressed");
+    }
+
+    public void OnStart() {
+        Debug.Log("Start Pressed");
+    }
+}
