106c464ab30bcc3641cf9e41cab81e6dd2c532dd
Benjamin <benjaminpalko@hotmail.com>
2023-09-18 15:49:51 -0400

    Create Abductable.cs and add script to PlayerController.cs

---
+++ Assets/Resources/Scripts/Abductable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Scripts {
+	public class Abductable : MonoBehaviour {
+
+		public bool immobilize;
+		
+		private void OnDestroy() {
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,14 +8,22 @@ namespace Scripts {
 	public class PlayerController : MonoBehaviour {
 
 		[SerializeField] private float movementSpeed = 1.0f;
+		[SerializeField] private float abductionSpeed = 1.0f;
 		[SerializeField] private GameObject beam;
-    
+
+		private Collider2D m_Collider;
+		private readonly List<Abductable> m_Abductables = new();
+		private Abductable m_Abductable;
 		private Vector3 m_Movement;
 		private bool m_Abduction;
-    
+
+		private void Awake() {
+			m_Collider = GetComponent<Collider2D>();
+		}
+
 		private void Update() {
 			if (m_Movement != Vector3.zero && !m_Abduction) Movement();
-			else if (m_Abduction) Abduct();
+			else if (m_Abduction && m_Abductable) Abduct();
 		}
 
 		public void OnMove(InputValue inputValue) {
@@ -22,6 +32,21 @@ namespace Scripts {
 
 		public void OnA(InputValue inputValue) {
 			m_Abduction = inputValue.isPressed;
+			if (m_Abduction) {
+				var minDistance = Mathf.Infinity;
+				var position = m_Collider.transform.position;
+				foreach (var abductable in m_Abductables) {
+					var abductablePosition = abductable.transform.position;
+					var distance = (position - abductablePosition).magnitude;
+					if (!(distance < minDistance)) continue;
+					minDistance = distance;
+					m_Abductable = abductable;
+				}
+				if (m_Abductable) m_Abductable.immobilize = true;
+			} else {
+				if (m_Abductable) m_Abductable.immobilize = false;
+				m_Abductable = null;
+			}
 			beam.SetActive(m_Abduction);
 		}
 
@@ -39,12 +64,30 @@ namespace Scripts {
     
 		private void Movement() {
 			var position = transform.position;
-			position = Vector3.MoveTowards(position, position + m_Movement,
+			position = Vector3.MoveTowards(position, position + new Vector3(m_Movement.x, m_Movement.y, position.z),
 				Time.deltaTime * movementSpeed);
 			transform.position = position;
 		}
 
 		private void Abduct() {
+			var position = transform.position;
+			var abductablePosition = m_Abductable.transform.position;
+			abductablePosition = Vector3.MoveTowards(abductablePosition, position, Time.deltaTime * abductionSpeed);
+			var distance = (abductablePosition - position).magnitude;
+			if (distance < 0.10f) Destroy(m_Abductable);
+			else m_Abductable.transform.position = abductablePosition;
+		}
+
+		private void OnTriggerEnter2D(Collider2D other) {
+			if (!other.TryGetComponent<Abductable>(out var abductable)) return;
+			Debug.Log("Enter: " + abductable.name);
+			m_Abductables.Add(abductable);
+		}
+
+		private void OnTriggerExit2D(Collider2D other) {
+			if (!other.TryGetComponent<Abductable>(out var abductable)) return;
+			Debug.Log("Leave: " + abductable.name);
+			m_Abductables.Remove(abductable);
 		}
 	}
 
