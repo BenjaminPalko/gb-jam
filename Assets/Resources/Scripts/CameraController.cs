5d4ea928eddca26b092303d708fb876a89ae44f1
Benjamin <benjaminpalko@hotmail.com>
2023-09-18 14:57:45 -0400

    scripts namespace

--- Assets/Resources/Scripts/CameraController.cs
+++ Assets/Resources/Scripts/CameraController.cs
@@ -1,55 +1,59 @@
 using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Camera))]
-public class CameraController : MonoBehaviour {
-    
-    public Target target {
-        set {
-            switch (value) {
-                case Target.None:
-                    m_TargetEnum = value;
-                    m_Target = null;
-                    break;
-                case Target.Player:
-                    m_TargetEnum = value;
-                    var playerController = FindObjectOfType<PlayerController>();
-                    if (playerController) {
-                        m_TargetEnum = value;
-                        m_Target = playerController.transform;
-                    } else {
-                        m_TargetEnum = Target.None;
-                        m_Target = null;
-                    }
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(value), value, "Invalid Target");
-            }
-        }
-    }
+namespace Scripts {
+	[RequireComponent(typeof(Camera))]
 
-    private Target m_TargetEnum;
-    private Transform m_Target;
-    
-    private Camera m_Camera;
+	public class CameraController : MonoBehaviour {
 
-    private void Awake() {
-        m_Camera = GetComponent<Camera>();
-    }
+		public Target target {
+			set {
+				switch (value) {
+					case Target.None:
+						m_TargetEnum = value;
+						m_Target = null;
+						break;
+					case Target.Player:
+						m_TargetEnum = value;
+						var playerController = FindObjectOfType<PlayerController>();
+						if (playerController) {
+							m_TargetEnum = value;
+							m_Target = playerController.transform;
+						} else {
+							m_TargetEnum = Target.None;
+							m_Target = null;
+						}
 
-    private void Start() {
-        target = Target.Player;
-    }
+						break;
+					default:
+						throw new ArgumentOutOfRangeException(nameof(value), value, "Invalid Target");
+				}
+			}
+		}
 
-    private void Update() {
-        if (!m_Target) return;
-        var targetPosition = m_Target.position;
-        var cameraTransform = transform;
-        cameraTransform.position = new Vector3(targetPosition.x, targetPosition.y, cameraTransform.position.z);
-    }
+		private Target m_TargetEnum;
+		private Transform m_Target;
 
-    public enum Target {
-        None,
-        Player
-    }
+		private Camera m_Camera;
+
+		private void Awake() {
+			m_Camera = GetComponent<Camera>();
+		}
+
+		private void Start() {
+			target = Target.Player;
+		}
+
+		private void Update() {
+			if (!m_Target) return;
+			var targetPosition = m_Target.position;
+			var cameraTransform = transform;
+			cameraTransform.position = new Vector3(targetPosition.x, targetPosition.y, cameraTransform.position.z);
+		}
+
+		public enum Target {
+			None,
+			Player
+		}
+	}
 }
--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -1,48 +1,51 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(PlayerInput))]
-public class PlayerController : MonoBehaviour {
+namespace Scripts {
+	[RequireComponent(typeof(PlayerInput))]
+	public class PlayerController : MonoBehaviour {
 
-    [SerializeField] private float movementSpeed = 1.0f;
-    [SerializeField] private GameObject beam;
+		[SerializeField] private float movementSpeed = 1.0f;
+		[SerializeField] private GameObject beam;
     
-    private Vector3 m_Movement;
-    private bool m_Abduction;
+		private Vector3 m_Movement;
+		private bool m_Abduction;
     
-    private void Update() {
-        if (m_Movement != Vector3.zero && !m_Abduction) Movement();
-        else if (m_Abduction) Abduct();
-    }
+		private void Update() {
+			if (m_Movement != Vector3.zero && !m_Abduction) Movement();
+			else if (m_Abduction) Abduct();
+		}
 
-    public void OnMove(InputValue inputValue) {
-        m_Movement = inputValue.Get<Vector2>();
-    }
+		public void OnMove(InputValue inputValue) {
+			m_Movement = inputValue.Get<Vector2>();
+		}
 
-    public void OnA(InputValue inputValue) {
-        m_Abduction = inputValue.isPressed;
-        beam.SetActive(m_Abduction);
-    }
+		public void OnA(InputValue inputValue) {
+			m_Abduction = inputValue.isPressed;
+			beam.SetActive(m_Abduction);
+		}
 
-    public void OnB() {
-        Debug.Log("B Pressed");
-    }
+		public void OnB() {
+			Debug.Log("B Pressed");
+		}
 
-    public void OnSelect() {
-        Debug.Log("Select Pressed");
-    }
+		public void OnSelect() {
+			Debug.Log("Select Pressed");
+		}
 
-    public void OnStart() {
-        Debug.Log("Start Pressed");
-    }
+		public void OnStart() {
+			Debug.Log("Start Pressed");
+		}
     
-    private void Movement() {
-        var position = transform.position;
-        position = Vector3.MoveTowards(position, position + m_Movement,
-            Time.deltaTime * movementSpeed);
-        transform.position = position;
-    }
+		private void Movement() {
+			var position = transform.position;
+			position = Vector3.MoveTowards(position, position + m_Movement,
+				Time.deltaTime * movementSpeed);
+			transform.position = position;
+		}
 
-    private void Abduct() {
-    }
+		private void Abduct() {
+		}
+	}
+
 }
