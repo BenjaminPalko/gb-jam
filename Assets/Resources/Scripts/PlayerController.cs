de398b52fd99e04c5b88ebc56f5118148ad62e90
Benjamin <benjaminpalko@hotmail.com>
2023-09-18 14:56:42 -0400

    Add beam when A is pressed

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -5,23 +5,23 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour {
 
     [SerializeField] private float movementSpeed = 1.0f;
+    [SerializeField] private GameObject beam;
     
     private Vector3 m_Movement;
+    private bool m_Abduction;
     
     private void Update() {
-        if (m_Movement == Vector3.zero) return;
-        var position = transform.position;
-        position = Vector3.MoveTowards(position, position + m_Movement,
-            Time.deltaTime * movementSpeed);
-        transform.position = position;
+        if (m_Movement != Vector3.zero && !m_Abduction) Movement();
+        else if (m_Abduction) Abduct();
     }
 
     public void OnMove(InputValue inputValue) {
         m_Movement = inputValue.Get<Vector2>();
     }
 
-    public void OnA() {
-        Debug.Log("A Pressed");
+    public void OnA(InputValue inputValue) {
+        m_Abduction = inputValue.isPressed;
+        beam.SetActive(m_Abduction);
     }
 
     public void OnB() {
@@ -34,5 +34,15 @@ public class PlayerController : MonoBehaviour {
 
     public void OnStart() {
         Debug.Log("Start Pressed");
+    }
+    
+    private void Movement() {
+        var position = transform.position;
+        position = Vector3.MoveTowards(position, position + m_Movement,
+            Time.deltaTime * movementSpeed);
+        transform.position = position;
+    }
+
+    private void Abduct() {
     }
 }
