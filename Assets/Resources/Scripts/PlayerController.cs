fcb83cb72f9c0a04b32ba38af7978b0e4166939a
Benjamin <benjaminpalko@hotmail.com>
2023-09-19 16:09:06 -0400

    use separate script for TargetReticle that keeps track of Abductables within the trigger

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,18 +5,16 @@ namespace Scripts {
 	[RequireComponent(typeof(PlayerInput))]
 	public class PlayerController : MonoBehaviour {
 		[SerializeField] private float movementSpeed = 1.0f;
-		[SerializeField] private float abductionSpeed = 1.0f;
-		[SerializeField] private GameObject beam;
 		[SerializeField] private Vector3 cargoOffset;
-		private readonly List<Abductable> m_Abductables = new();
 		private Abductable m_Abductable;
 
-		private Collider2D m_Collider;
 		private bool m_Immobilize;
 		private Vector3 m_Movement;
+		private TargetReticle m_TargetReticle;
 
 		private void Awake() {
-			m_Collider = GetComponent<Collider2D>();
+			m_TargetReticle = GetComponentInChildren<TargetReticle>();
+			if (!m_TargetReticle) Debug.LogError("TargetReticle not found on child component!");
 		}
 
 		private void Update() {
@@ -30,16 +27,6 @@ namespace Scripts {
 			Gizmos.DrawLine(position - Vector3.left * 0.2f, position - Vector3.right * 0.2f);
 		}
 
-		private void OnTriggerEnter2D(Collider2D other) {
-			if (!other.TryGetComponent<Abductable>(out var abductable)) return;
-			m_Abductables.Add(abductable);
-		}
-
-		private void OnTriggerExit2D(Collider2D other) {
-			if (!other.TryGetComponent<Abductable>(out var abductable)) return;
-			m_Abductables.Remove(abductable);
-		}
-
 		public void OnMove(InputValue inputValue) {
 			m_Movement = inputValue.Get<Vector2>();
 		}
@@ -48,8 +35,8 @@ namespace Scripts {
 			m_Immobilize = inputValue.isPressed;
 			if (m_Immobilize) {
 				var minDistance = Mathf.Infinity;
-				var position = m_Collider.transform.position;
-				foreach (var abductable in m_Abductables) {
+				var position = m_TargetReticle.transform.position;
+				foreach (var abductable in m_TargetReticle.Abductables) {
 					var abductablePosition = abductable.transform.position;
 					var distance = (position - abductablePosition).magnitude;
 					if (!(distance < minDistance)) continue;
@@ -62,8 +49,6 @@ namespace Scripts {
 				m_Abductable.StopAbduction();
 				m_Abductable = null;
 			}
-
-			beam.SetActive(m_Immobilize);
 		}
 
 		public void OnB() {
