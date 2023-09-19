5d5069e5c315b4d71b243b00a15f789ef636cef0
Benjamin <benjaminpalko@hotmail.com>
2023-09-18 20:26:47 -0400

    Add cow prefab

--- Assets/Resources/Scripts/Abductable.cs
+++ Assets/Resources/Scripts/Abductable.cs
@@ -1,10 +1,20 @@
 ﻿using UnityEngine;
 
 namespace Scripts {
+	[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D), typeof(Animator))]
 	public class Abductable : MonoBehaviour {
-
 		public bool immobilize;
-		
+
+		private Animator m_Animator;
+
+		private void Awake() {
+			m_Animator = GetComponent<Animator>();
+		}
+
+		private void Start() {
+			m_Animator.speed = 0.25f;
+		}
+
 		private void OnDestroy() {
 			Destroy(gameObject);
 		}
