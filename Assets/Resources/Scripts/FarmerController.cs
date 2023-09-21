3f7a155e9f569f3be246e6cd689db43a4812d4c4
Benjamin <benjaminpalko@hotmail.com>
2023-09-21 16:32:26 -0400

    FarmerController.cs

---
+++ Assets/Resources/Scripts/FarmerController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+namespace Scripts {
+	[RequireComponent(typeof(SpriteRenderer), typeof(Animator))]
+	public class FarmerController : MonoBehaviour {
+		private static readonly int Property = Animator.StringToHash("throw");
+		private static readonly int Aggressive = Animator.StringToHash("aggressive");
+
+		[SerializeField] private float speed = 0.5f;
+		[SerializeField] private float fireRate = 0.25f;
+
+		[SerializeField] private GameObject pitchFork;
+
+		[SerializeField] private FarmerTrigger alertTrigger;
+		[SerializeField] private FarmerTrigger attackTrigger;
+		[SerializeField] private FarmerTrigger chaseTrigger;
+		private bool m_Aggravated;
+		private Animator m_Animator;
+		private float m_Cooldown;
+		private PitchforkController m_PitchforkController;
+
+		private SpriteRenderer m_SpriteRenderer;
+
+		private void Awake() {
+			m_SpriteRenderer = GetComponent<SpriteRenderer>();
+			m_Animator = GetComponent<Animator>();
+		}
+
+		private void Update() {
+			if (m_Cooldown > 0.0f) m_Cooldown -= Time.deltaTime;
+			if (attackTrigger.playerController) {
+				Attack(attackTrigger.playerController.transform.position);
+			} else if (alertTrigger.playerController && alertTrigger.playerController.immobilize) {
+				if (!m_Aggravated) Chase();
+				Move(alertTrigger.playerController.transform.position);
+			} else if (chaseTrigger.playerController) {
+				if (!m_Aggravated) Chase();
+				Move(chaseTrigger.playerController.transform.position);
+			} else if (m_Aggravated) {
+				Calm();
+			}
+		}
+
+		private void Move(Vector2 destination) {
+			var diff = destination - (Vector2)transform.position;
+			if (diff != Vector2.zero) m_SpriteRenderer.flipX = diff.x < 0;
+			transform.position = Vector2.MoveTowards(transform.position, destination, Time.deltaTime * speed);
+		}
+
+		private void Chase() {
+			m_Aggravated = true;
+			m_Animator.SetBool(Aggressive, m_Aggravated);
+		}
+
+		private void Calm() {
+			if (m_PitchforkController) Destroy(m_PitchforkController);
+			m_PitchforkController = null;
+			m_Aggravated = false;
+			m_Animator.SetBool(Aggressive, m_Aggravated);
+		}
+
+		private void Attack(Vector2 target) {
+			if (m_Cooldown > 0 || m_PitchforkController) return;
+			m_PitchforkController = Instantiate(pitchFork, transform).GetComponent<PitchforkController>();
+			m_Animator.SetTrigger(Property);
+			m_PitchforkController.Throw(target);
+			m_Cooldown = 1 / fireRate;
+		}
+	}
+}
--- Assets/Resources/Scripts/PitchforkController.cs
+++ Assets/Resources/Scripts/PitchforkController.cs
@@ -2,21 +2,25 @@ using System.Collections;
 using UnityEngine;
 
 namespace Scripts {
-	[RequireComponent(typeof(Animator), typeof(Rigidbody2D))]
+	[RequireComponent(typeof(SpriteRenderer), typeof(Animator), typeof(Rigidbody2D))]
 	public class PitchforkController : MonoBehaviour {
 		private static readonly int Speed = Animator.StringToHash("speed");
 
 		[SerializeField] private float despawnDelay = 2.0f;
-
+		[SerializeField] private float collisionDeviation = 0.75f;
 		private Animator m_Animator;
 
 		private IEnumerator m_DespawnCoroutine;
 		private Vector2 m_InitialPosition;
 		private Vector2 m_InitialVelocity;
 		private Rigidbody2D m_Rigidbody2D;
+
+		private SpriteRenderer m_SpriteRenderer;
+		private Vector2 m_Target;
 		private bool m_Throw;
 
 		private void Awake() {
+			m_SpriteRenderer = GetComponent<SpriteRenderer>();
 			m_Animator = GetComponent<Animator>();
 			m_Rigidbody2D = GetComponent<Rigidbody2D>();
 			m_Rigidbody2D.bodyType = RigidbodyType2D.Kinematic;
@@ -43,22 +47,30 @@ namespace Scripts {
 		}
 
 		private void OnCollisionEnter2D(Collision2D other) {
-			if (other.gameObject.TryGetComponent<PlayerController>(out _)) Destroy(this);
+			if (other.gameObject.TryGetComponent<PlayerController>(out _)) {
+				var collided = Mathf.Abs(transform.position.y - m_Target.y) < collisionDeviation &&
+				               Mathf.Abs(transform.position.x - m_Target.x) < collisionDeviation;
+				if (collided) Destroy(this);
+			}
 		}
 
 		public void Throw(Vector2 target) {
 			m_Throw = true;
+			m_Target = target;
 			m_InitialPosition = transform.position;
+			var diff = m_Target - m_InitialPosition;
+			if (diff != Vector2.zero)
+				m_SpriteRenderer.flipX = diff.x < 0 ? !m_SpriteRenderer.flipX : m_SpriteRenderer.flipX;
 			m_InitialVelocity = CalculateInitialVelocity();
 			m_Rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
 			m_Rigidbody2D.velocity = m_InitialVelocity;
 			return;
 
 			Vector2 CalculateInitialVelocity() {
-				const float time = 1.0f;
+				const float time = 1.5f;
 				var position = transform.position;
-				return new Vector2((target.x - position.x) / time, Mathf.Sqrt(2 * -Physics.gravity.y *
-				                                                              (target.y - position.y)));
+				return new Vector2((m_Target.x - position.x) / time, Mathf.Sqrt(2 * -Physics.gravity.y *
+					(m_Target.y - position.y)));
 			}
 		}
 
