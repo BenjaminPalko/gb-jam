860833de3d222d97440f0ef55a19b523681490be
Benjamin <benjaminpalko@hotmail.com>
2023-09-22 18:05:05 -0400

    Stop cow movement when abduction

--- Assets/Resources/Scripts/Abductable.cs
+++ Assets/Resources/Scripts/Abductable.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
 
 namespace Scripts {
-	[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
+	[
+		RequireComponent(typeof(Collider2D), typeof(Rigidbody2D), typeof(Animator)),
+		RequireComponent(typeof(CowBehaviour))
+	]
 	public class Abductable : MonoBehaviour {
+		private static readonly int Abducting = Animator.StringToHash("abducting");
 		[SerializeField] private float liftSpeed = 1.0f;
 		[SerializeField] private float fallSpeed = 1.0f;
 
+		private Animator m_Animator;
 		private Vector3 m_AttractorPosition;
+		private CowBehaviour m_CowBehaviour;
 		private bool m_Immobilize;
 		private Vector3 m_OriginalPosition;
+
+		private void Awake() {
+			m_Animator = GetComponent<Animator>();
+			m_CowBehaviour = GetComponent<CowBehaviour>();
+		}
 
 		private void Update() {
 			if (m_AttractorPosition != Vector3.zero && m_Immobilize) Attract();
@@ -20,12 +31,15 @@ namespace Scripts {
 		}
 
 		public void StartAbduction(Vector3 saucerPosition) {
+			m_Animator.SetBool(Abducting, true);
+			m_CowBehaviour.enabled = false;
 			m_OriginalPosition = transform.position;
 			m_AttractorPosition = saucerPosition;
 			m_Immobilize = true;
 		}
 
 		public void StopAbduction() {
+			m_Animator.SetBool(Abducting, false);
 			m_AttractorPosition = Vector3.zero;
 			m_OriginalPosition.x = transform.position.x;
 		}
@@ -50,6 +64,7 @@ namespace Scripts {
 
 		private void Ground() {
 			m_Immobilize = false;
+			m_CowBehaviour.enabled = true;
 		}
 	}
 }
--- Assets/Resources/Scripts/CowBehaviour.cs
+++ Assets/Resources/Scripts/CowBehaviour.cs
@@ -2,61 +2,86 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class CowBehaviour : MonoBehaviour {
-	private static readonly int Speed = Animator.StringToHash("speed");
-	private static readonly int Graze = Animator.StringToHash("graze");
-	[SerializeField] private int wanderRadius = 4;
-	private Animator m_Animator;
-	private IEnumerator m_MovementCoroutine;
+namespace Scripts {
+	[RequireComponent(typeof(SpriteRenderer), typeof(Animator), typeof(NavMeshAgent))]
+	public class CowBehaviour : MonoBehaviour {
+		private static readonly int Speed = Animator.StringToHash("speed");
+		private static readonly int Graze = Animator.StringToHash("graze");
 
-	private NavMeshAgent m_NavMeshAgent;
-	private SpriteRenderer m_SpriteRenderer;
+		[SerializeField] private int wanderRadius = 4;
+		private Animator m_Animator;
+		private NavMeshAgent m_NavMeshAgent;
 
+		private SpriteRenderer m_SpriteRenderer;
+		private IEnumerator m_WanderCoroutine;
 
-	private void Awake() {
-		m_NavMeshAgent = GetComponent<NavMeshAgent>();
-		m_SpriteRenderer = GetComponent<SpriteRenderer>();
-		m_Animator = GetComponent<Animator>();
-	}
-
-	private void Start() {
-		m_Animator.SetInteger(Speed, 0);
-		m_NavMeshAgent.updateUpAxis = false;
-		m_NavMeshAgent.updateRotation = false;
-		m_MovementCoroutine = NewHeading();
-		StartCoroutine(m_MovementCoroutine);
-	}
-
-
-	private void Update() {
-		if (m_NavMeshAgent.velocity != Vector3.zero) {
-			m_SpriteRenderer.flipX = m_NavMeshAgent.velocity.x < 0;
+		private void Awake() {
+			m_NavMeshAgent = GetComponent<NavMeshAgent>();
+			m_SpriteRenderer = GetComponent<SpriteRenderer>();
+			m_Animator = GetComponent<Animator>();
 		}
-	}
 
-	private void OnDestroy() {
-		StopCoroutine(m_MovementCoroutine);
-	}
-
-	private static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask) {
-		var randDirection = Random.insideUnitSphere * dist;
-		randDirection += origin;
-		return NavMesh.SamplePosition(randDirection, out var navHit, dist, layermask) ? navHit.position : origin;
-	}
-
-	private IEnumerator NewHeading() {
-		while (true) {
-			var destination = RandomNavSphere(transform.position, wanderRadius, -1);
-			if (destination != transform.position) {
-				m_NavMeshAgent.SetDestination(destination);
-			}
-
-			yield return new WaitUntil(() => m_NavMeshAgent.velocity != Vector3.zero);
-			m_Animator.SetInteger(Speed, 1);
-			yield return new WaitUntil(() => m_NavMeshAgent.velocity == Vector3.zero);
+		private void Start() {
 			m_Animator.SetInteger(Speed, 0);
-			m_Animator.SetTrigger(Graze);
-			yield return new WaitForSecondsRealtime(Random.Range(2.0f, 5.0f));
+			m_NavMeshAgent.updateUpAxis = false;
+			m_NavMeshAgent.updateRotation = false;
+			StartWander();
+		}
+
+
+		private void Update() {
+			if (m_NavMeshAgent.velocity != Vector3.zero) {
+				m_SpriteRenderer.flipX = m_NavMeshAgent.velocity.x < 0;
+			}
+		}
+
+		private void OnEnable() {
+			StartWander();
+		}
+
+		private void OnDisable() {
+			StopWander();
+		}
+
+		private void OnDestroy() {
+			StartWander();
+		}
+
+		private static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask) {
+			var randDirection = Random.insideUnitSphere * dist;
+			randDirection += origin;
+			return NavMesh.SamplePosition(randDirection, out var navHit, dist, layermask) ? navHit.position : origin;
+		}
+
+		private IEnumerator Wander() {
+			while (true) {
+				var destination = RandomNavSphere(transform.position, wanderRadius, -1);
+				if (destination != transform.position) {
+					m_NavMeshAgent.SetDestination(destination);
+				}
+
+				yield return new WaitUntil(() => m_NavMeshAgent.velocity != Vector3.zero);
+				m_Animator.SetInteger(Speed, 1);
+				yield return new WaitUntil(() => m_NavMeshAgent.velocity == Vector3.zero);
+				m_Animator.SetInteger(Speed, 0);
+				var wait = Random.Range(2.0f, 5.0f);
+				yield return new WaitForSecondsRealtime(wait / 2);
+				m_Animator.SetTrigger(Graze);
+				yield return new WaitForSecondsRealtime(wait / 2);
+			}
+		}
+
+		private void StartWander() {
+			if (m_WanderCoroutine != null) return;
+			m_WanderCoroutine = Wander();
+			StartCoroutine(m_WanderCoroutine);
+		}
+
+		private void StopWander() {
+			if (m_WanderCoroutine == null) return;
+			StopCoroutine(m_WanderCoroutine);
+			m_NavMeshAgent.ResetPath();
+			m_WanderCoroutine = null;
 		}
 	}
 }
